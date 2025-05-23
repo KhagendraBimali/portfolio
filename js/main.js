26e6965a27272cacf2f605ed74c33eb47330a705
Khagendra Bimali <khangendrapdbimali911@gmail.com>
2025-05-23 18:06:35 +0545

    minor changes

--- js/main.js
+++ js/main.js
@@ -218,50 +218,32 @@
 
 	/* local validation */
 	$('#contactForm').validate({
-
 		/* submit via ajax */
 		submitHandler: function(form) {
-
 			var sLoader = $('#submit-loader');
+			var formData = $(form).serialize();
 
 			$.ajax({      	
-
-		      type: "POST",
-		      url: "inc/sendEmail.php",
-		      data: $(form).serialize(),
-		      beforeSend: function() { 
-
-		      	sLoader.fadeIn(); 
-
-		      },
-		      success: function(msg) {
-
-	            // Message was sent
-	            if (msg == 'OK') {
-	            	sLoader.fadeOut(); 
-	               $('#message-warning').hide();
-	               $('#contactForm').fadeOut();
-	               $('#message-success').fadeIn();   
-	            }
-	            // There was an error
-	            else {
-	            	sLoader.fadeOut(); 
-	               $('#message-warning').html(msg);
-		            $('#message-warning').fadeIn();
-	            }
-
-		      },
-		      error: function() {
-
-		      	sLoader.fadeOut(); 
-		      	$('#message-warning').html("Something went wrong. Please try again.");
-		         $('#message-warning').fadeIn();
-
-		      }
-
-	      });     		
-  		}
-
+				type: "POST",
+				url: $(form).attr('action'),
+				data: formData,
+				beforeSend: function() { 
+					sLoader.fadeIn(); 
+					$('#message-warning').hide();
+					$('#message-success').hide();
+				},
+				success: function(msg) {
+					sLoader.fadeOut(); 
+					$('#contactForm').fadeOut();
+					$('#message-success').fadeIn();   
+				},
+				error: function() {
+					sLoader.fadeOut(); 
+					$('#message-warning').html("Something went wrong. Please try again.");
+					$('#message-warning').fadeIn();
+				}
+			});     		
+		}
 	});
 
 
